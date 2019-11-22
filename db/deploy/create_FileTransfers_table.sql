c6f7ff9ad28d4c08bd25fd0278567bfc1d1a7f60
Mase <andrew@nocturnal.net.au>
2019-11-22 11:34:15 +1030

    Really use utf8, not the fake utf8

--- db/deploy/create_FileTransfers_table.sql
+++ db/deploy/create_FileTransfers_table.sql
@@ -24,4 +24,4 @@ CREATE TABLE TransferRecord (
     
     UNIQUE INDEX file_correlationID USING HASH (local_file_name,correlation_id)
 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
