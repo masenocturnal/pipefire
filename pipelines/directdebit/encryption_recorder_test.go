3dd68337bc6805a314567f1a36d2cff98775ef2f
Mase <andrew@nocturnal.net.au>
2019-12-09 10:44:19 +1030

    More tests

--- pipelines/directdebit/encryption_recorder_test.go
+++ pipelines/directdebit/encryption_recorder_test.go
@@ -2,34 +2,22 @@ package directdebit
 
 import (
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
-var configPath string = "../config/pipefired.json"
-
-// func setup(config string) (Pipeline, error) {
-
-// hostConfig, err := config.ReadApplicationConfig("pipefired")
-// if err != nil {
-// 	return err
-// }
-
-// // @todo make this dynamic
-// ddConfig := hostConfig.Pipelines.DirectDebit
-
-// // create the dd pipeline
-// directDebitPipeline, err := New(&ddConfig)
-// if err != nil {
-// 	log.Error(err.Error())
-// 	os.Exit(1)
-// }
-// return directDebitPipeline, err
-// }
-
 func TestRecordEncryptionCreate(t *testing.T) {
-	// pipeline, err := setup(configPath)
-	// if err != nil {
-	// 	t.Error(err.Error())
-	// }
+	pipelineConfig, err := setup(t)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	db, err := connectToDb(pipelineConfig.Database)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	recorder := NewEncryptionRecorder(db, log.WithField("test", "true"))
 
 }
 
